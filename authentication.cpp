c6ec2319a6699770a615d3ad1754fed663283764
megatgotouchgrass <megatmuhammadzafran@graduate.utm.my>
2023-06-03 03:20:23 +0800

    sign up function done

--- authentication.cpp
+++ authentication.cpp
@@ -1,8 +1,52 @@
 #include "authentication.h"
 #include <iostream>
+#include <conio.h>
+#include <string>
 
 using namespace std;
 
+enum TT_Input
+{
+
+    // ASCII code of backspace is 8
+    BACKSPACE = 8,
+    RETURN = 32
+};
+
+string Authentication::passwordInput()
+{
+    string ipt = "";
+    char ipt_ch;
+    while (true)
+    {
+        ipt_ch = getch();
+
+        if (ipt_ch < TT_Input::RETURN && ipt_ch != TT_Input::BACKSPACE)
+        {
+            cout << endl;
+            return ipt;
+        }
+
+        // Check whether the user
+        // pressed backspace
+        if (ipt_ch == TT_Input::BACKSPACE)
+        {
+
+            // Check if ipt is empty or not
+            if (ipt.length() == 0)
+                continue;
+            else
+            {
+
+                // Removes last character
+                ipt.pop_back();
+
+                continue;
+            }
+        }
+        ipt.push_back(ipt_ch);
+    }
+}
 void Authentication::userSignUp()
 {
     greetings();
@@ -13,6 +57,14 @@ void Authentication::userSignUp()
 
     if (continueOperation == 'Y' || continueOperation == 'y')
     {
+        greetings();
+        cout << "Enter your username: ";
+        cin >> username;
+
+        cout << "Enter your password: ";
+        password = passwordInput();
+
+        fflush(stdin);
     }
 };
 
--- authentication.h
+++ authentication.h
@@ -1,12 +1,20 @@
 #ifndef AUTHENTICATION_H
 #define AUTHENTICATION_H
 #include "init.h"
+#include <string>
+
+using namespace std;
 
 class Authentication : public Init
 {
+private:
+    string username;
+    string password;
+
 public:
     void userSignUp();
     void userLogIn();
+    string passwordInput();
 };
 
 #endif
